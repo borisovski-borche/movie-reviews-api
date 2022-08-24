a724d2c71757d4a93528e3b19d9cdcf0c14cbc28
AlamoFiteCat <shredgod_lobos@hotmail.com>
2022-08-24 23:23:04 +0200

    Movies work with typeORM

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateMovieDto } from './create-movie.dto';
 import { MoviesService } from './movies.service';
+import { UpdateMovieDto } from './update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -13,5 +23,20 @@ export class MoviesController {
   @Get('/:id')
   getMovieById(@Param('id') id: string) {
     return this.moviesService.getMovieById(parseInt(id));
+  }
+
+  @Post('/')
+  async createNewMovie(@Body() body: CreateMovieDto) {
+    return await this.moviesService.createNewMovie(body);
+  }
+
+  @Patch('/:id')
+  updateMovie(@Param('id') id: string, @Body() body: UpdateMovieDto) {
+    return this.moviesService.updateMovie(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  deleteMovie(@Param('id') id: string) {
+    return this.moviesService.deleteMovie(parseInt(id));
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,101 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { text } from 'stream/consumers';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateMovieDto } from './create-movie.dto';
 import { Movie } from './movie.entity';
-
-const movieList: Movie[] = [
-  {
-    id: 1,
-    title: 'Star Wars: The Phantom Menace',
-    year: 1999,
-    genres: 'sci-fi,fantasy',
-    rating: 6,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Ivan',
-    likeCount: 0,
-    director: 'George Lucas',
-  },
-  {
-    id: 2,
-    title: 'Lord of the Rings: Fellowship of the Ring',
-    year: 1999,
-    genres: 'adventure,fantasy',
-    rating: 8,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Ivan',
-    likeCount: 0,
-    director: 'Peter Jackson',
-  },
-  {
-    id: 3,
-    title: 'Lord of the Rings: Two Towers',
-    year: 2001,
-    genres: 'adventure,fantasy',
-    rating: 8,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Ivan',
-    likeCount: 0,
-    director: 'Peter Jackson',
-  },
-  {
-    id: 4,
-    title: 'Lord of the Rings: Return of the King',
-    year: 2003,
-    genres: 'adventure,fantasy',
-    rating: 10,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Borche',
-    likeCount: 0,
-    director: 'Peter Jackson',
-  },
-  {
-    id: 5,
-    title: '12 Angry Men',
-    year: 1959,
-    genres: 'sci-fi,thriller',
-    rating: 9,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Borche',
-    likeCount: 0,
-    director: 'Sydney Lumet',
-  },
-  {
-    id: 6,
-    title: 'Saving Private Ryan',
-    year: 1999,
-    genres: 'war,action',
-    rating: 10,
-    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-    maiores impedit optio voluptates placeat! Reiciendis aperiam illo,
-    deserunt, fuga expedita quia voluptatibus deleniti rerum impedit amet
-    culpa maxime voluptate atque?`,
-    author: 'Borche',
-    likeCount: 0,
-    director: 'Steven Spielberg',
-  },
-];
 
 @Injectable()
 export class MoviesService {
-  getAllMovies(): Movie[] {
-    return movieList;
+  constructor(@InjectRepository(Movie) private repo: Repository<Movie>) {}
+  getAllMovies() {
+    return this.repo.find();
   }
 
-  getMovieById(id: number): Movie {
-    return movieList.filter((movie: Movie) => movie.id === id)[0];
+  getMovieById(movieId: number) {
+    const foundMovie = this.repo.findOne({
+      where: {
+        id: movieId,
+      },
+    });
+    if (!foundMovie) {
+      throw new NotFoundException('Movie not found!');
+    }
+    return foundMovie;
+  }
+
+  createNewMovie(movieDto: CreateMovieDto) {
+    const newMovie = this.repo.create(movieDto);
+    return this.repo.save(newMovie);
+  }
+
+  async updateMovie(movieId: number, attrs: Partial<Movie>) {
+    const movie = await this.getMovieById(movieId);
+    Object.assign(movie, attrs);
+    console.log(movie);
+    return this.repo.save(movie);
+  }
+
+  async deleteMovie(movieId: number) {
+    const movie = await this.getMovieById(movieId);
+    return this.repo.remove(movie);
   }
 }
