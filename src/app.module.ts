50ae7985501bcb1e26d7e73b59e64e843a8ad606
borisovski-borche <borisovski.borce@gmail.com>
2024-08-15 17:01:19 +0200

    2024 update

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,14 +23,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           type: 'postgres',
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
           database: configService.get('DB_NAME'),
           synchronize: true,
           autoLoadEntities: true,
-          extra: {
-            ssl: true,
-          },
         };
       },
     }),
